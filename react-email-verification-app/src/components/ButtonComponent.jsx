ca5c61e37c38e5e487d6eeb7b84ac18b4cb7339a
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-26 08:55:40 +0530

    Update button's properties

--- react-email-verification-app/src/components/ButtonComponent.jsx
+++ react-email-verification-app/src/components/ButtonComponent.jsx
@@ -6,22 +6,24 @@ function ButtonComponent(props) {
     const secondaryButtonStyles = "border border-purple-400 hover:border-purple-500 outline-none active:bg-gray-100 text-purple-600 rounded-md px-3 py-1 w-80 mt-2 grid";
 
     return (
-        <button className={(props.isPrimary) ? primaryButtonStyles : secondaryButtonStyles}>
-            <Link to={props.href}>{props.text}</Link>
+        <button className={(props.isPrimary) ? primaryButtonStyles : secondaryButtonStyles} type={props.type}>
+            {(props.href.trim() != "") ? <Link to={props.href}>{props.text}</Link> : props.text}
         </button>
     );
 }
 
 ButtonComponent.propTypes = {
     text: PropTypes.string,
+    type: PropTypes.string,
     isPrimary: PropTypes.bool,
     href: PropTypes.string,
 }
 
 ButtonComponent.defaultProps = {
     text: "Click Here",
+    type: "",
     isPrimary: true,
-    href: "/",
+    href: "",
 }
 
 export default ButtonComponent;
