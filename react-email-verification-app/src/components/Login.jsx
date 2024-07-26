f9d9d41f33ef56b9b9178da4d2fb8773e2566a49
AadhilMR <aadhil2001ahamed@gmail.com>
2024-07-26 10:48:50 +0530

    Update code for send request to backend

--- react-email-verification-app/src/components/Login.jsx
+++ react-email-verification-app/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 import InputComponent from "./InputComponent";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -9,6 +10,7 @@ function Login() {
     });
 
     const [error, setError] = useState("");
+    const navigater = useNavigate();
 
     function handleChange(e) {
         const {name, value} = e.target;
@@ -25,19 +27,48 @@ function Login() {
         }
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!error.trim()) {
+            try {
+                const response = await fetch("http://localhost:8080/apiv2/login", {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(formData),
+                });
+
+                const message = await response.text();
+
+                if(response.ok) {
+                    console.log("Form submitted successfully: ", response);
+                    navigater("/");
+                } else {
+                    console.log("Error on submitting form: ", response);
+                    setError(message);
+                }
+            } catch(err) {
+                console.log("Error on submitting form err: ", err);
+                setError(err.text());
+            }
+        }
+    }
+
     return (
         <div className="rounded-md bg-white pt-8 pb-12 px-6 shadow-2xl">
-            <p className="text-2xl font-semibold mb-5">Login Here</p>
-            <InputComponent type="text" placeholder="Enter your email" name={"email"} onChange={handleChange} />
-            <br />
-            <InputComponent type="password" placeholder="Enter your password" name={"password"} onChange={handleChange} />
-            <br />
-            <br />
-            {error && <p className="text-red-500 text-xs w-80">{error}</p>}
-            <ButtonComponent isPrimary={true} text="Sign In" href="#" />
-            <br />
-            <p className="text-start mt-2 text-gray-500 italic text-sm">Do not have an account? </p>
-            <ButtonComponent isPrimary={false} text="Register Now" href="/register" />
+            <form onSubmit={handleSubmit}>
+                <p className="text-2xl font-semibold mb-5">Login Here</p>
+                <InputComponent type="text" placeholder="Enter your email" name={"email"} onChange={handleChange} />
+                <br />
+                <InputComponent type="password" placeholder="Enter your password" name={"password"} onChange={handleChange} />
+                <br />
+                <br />
+                {error && <p className="text-red-500 text-xs w-80">{error}</p>}
+                <ButtonComponent isPrimary={true} text="Sign In" type="submit" />
+                <br />
+                <p className="text-start mt-2 text-gray-500 italic text-sm">Do not have an account? </p>
+                <ButtonComponent isPrimary={false} text="Register Now" href="/register" />
+            </form>
         </div>
     );
 }
